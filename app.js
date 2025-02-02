a8fcabe3078d10be0955c9c52c3e9c2876a31b95
Sguti06 <sanguti06@gmail.com>
2025-02-01 19:28:18 -0500

    cambios para funcionamiento de back

--- app.js
+++ app.js
@@ -17,6 +17,9 @@ app.use(cors());
 app.use(express.json());
 app.use('/usuarios', usersRouter);
 app.use('/iniciarSesion', loginRouter);
+app.get("/",(req, res)=>{
+    res.send("Backend funcionando correctamente")
+});
 
 
 export default app;
